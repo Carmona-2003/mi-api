8c5eb28792bbc6ea5aa3bf7c95be2c798126f9c9
carmona-2003 <samuelcarmona08@gmail.com>
2025-12-14 22:42:49 -0500

    mailtrap4

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -1,16 +1,27 @@
-const nodemailer = require("nodemailer");
+import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
-  port: Number(process.env.MAIL_PORT),
-  secure: false, // Mailtrap usa STARTTLS
+  port: process.env.MAIL_PORT,
+  secure: false,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
   },
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
 });
 
-module.exports = transporter;
+export const sendRecoveryEmail = async (to, token) => {
+  const link = `${process.env.FRONT_URL}/reset-password?token=${token}`;
+
+  await transporter.sendMail({
+    from: "JireSoft <no-reply@jiresoft.com>",
+    to,
+    subject: "Recuperar contraseña",
+    html: `
+      <h2>Recuperación de contraseña</h2>
+      <p>Haz clic en el enlace para restablecer tu contraseña:</p>
+      <a href="${link}">${link}</a>
+      <p>Este enlace expira en 15 minutos.</p>
+    `,
+  });
+};
